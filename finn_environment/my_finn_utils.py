2d74af0fc16237971a93a55da94c3fdccfa8386a
Jose Vitor <josevfsilveira@gmail.com>
2026-06-21 19:46:15 -0300

    Complete the tidy up and pre processing sections of the FINN Framework. Added more functions on the my_finn_utils.py file to implement/adapt functions of the train_environment inside the finn_environment.

--- finn_environment/my_finn_utils.py
+++ finn_environment/my_finn_utils.py
@@ -1,9 +1,39 @@
 import torch
+from torchvision.datasets import Cityscapes
 from pathlib import Path
 import warnings
 
-def load_state_dict(model: torch.nn.Module, path: str, strict: bool = True, ignore_key_name: list=None) -> torch.nn.Module | tuple[torch.nn.Module, dict]:
+from config import IGNORE_INDEX, NUM_CLASSES
 
+
+def generate_cityscapes_labels():
+    '''
+    Function created based on the __init__() function of class CityscapesLables from train_environment/custom_cityscapes.py
+    '''
+
+    # Criando listas dos nomes e das cores das classes treinaveis
+    id_names = {}
+    color_list = []
+    lable_conversion = {}
+    for c in Cityscapes.classes:
+
+        # Adicionando valores ao dicionario de conversao de ids
+        lable_conversion[c.id] = c.train_id if c.train_id != -1 else IGNORE_INDEX # A classe 'ignore' tem train_id -1, entao atribui o valor IGNORE_INDEX para ela
+        # Adicionando valores as listas de nomes e cores
+        if c.train_id != -1 and c.train_id != 255:
+            id_names[c.train_id] = c.name
+            color_list.append(c.color)
+
+    # Variavel para dicionario de nomes
+    id_names.update({IGNORE_INDEX: 'ignore'}) # Adiciona a classe 'ignore' com train_id 255
+
+    return lable_conversion, id_names
+
+
+def load_state_dict(model: torch.nn.Module, path: str, strict: bool = True, ignore_key_name: list=None) -> torch.nn.Module | tuple[torch.nn.Module, dict]:
+    '''
+    Function copied from train_environment/utils.py
+    '''
     # Carregando apenas os parametros (state_dict()), pois isso flexibiliza o modelo e evita erros de incompatibilidade com parametros e caminhos do modelo original
     # OBS: torch.load() carrega o modelo inteiro, nao apenas os parametros
     path = Path(path)
@@ -53,3 +83,22 @@ def load_state_dict(model: torch.nn.Module, path: str, strict: bool = True, igno
         print(f"Pesos do modelo {model_name} nao encontrados.")
 
     return model
+
+
+class IdToTrainIdTransform:
+    '''
+    Class copied from train_environment/custom_transforms.py
+    '''
+    def __init__(self, conv_dict: dict):
+        self.conv_dict = conv_dict
+
+    def __call__(self, mask: torch.Tensor) -> torch.Tensor:
+        new_mask = mask # cria uma copia da mascara original para ser modificada
+        # Troca os valores da mascara original a partir do dicionario
+        for lable, new_lable in list(self.conv_dict.items()):
+            # Caso o pixel esteja com a mascara da key, troca para a mascara do value, caso contrario mantem a mascara original
+            new_mask = torch.where(mask == lable, new_lable, new_mask)
+        return new_mask
+    
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}()"
